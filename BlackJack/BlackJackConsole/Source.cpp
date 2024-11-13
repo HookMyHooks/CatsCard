fe5b542a64eaa21eec3c81c1ebf75b906df47d95
AlexiaMihaela <bordeaalexia24@gmail.com>
2024-11-13 13:25:34 +0200

    Created project for console

---
+++ BlackJack/BlackJackConsole/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Deck.h"
+#include "IGame.h"
+
+int main()
+{/*
+	Card card (EValue::eight, ENumber::seven);
+	ENumber value = card.GetNumber();
+	std::cout << (int)value;*/
+
+	Deck deck;
+	int count = 1;
+
+	for (auto& el : deck.GetCards())
+	{
+		ENumber number = el->GetNumber();
+		EValue value = el->GetValue();
+
+		std::cout << "Count: " << count << " with value: " << (int)value << " and Number: " << (int)number << std::endl;
+		count++;
+	}
+
+	/*auto game = IGame::Produce();
+	int count = 1;
+
+	for (auto& el : game->GetDeck())
+	{
+		ENumber number = el->GetNumber();
+		EValue value = el->GetValue();
+
+		std::cout << "Count: " << count << " with value: " << (int)value << " and Number: " << (int)number << std::endl;
+		count++;
+	}*/
+
+	return 0;
+}
