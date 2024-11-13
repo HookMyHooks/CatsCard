d9463028bebfff0575006a0eeb073703f4deba06
AlexiaMihaela <bordeaalexia24@gmail.com>
2024-11-13 10:41:17 +0200

    Moved function for convert out of the class

--- BlackJack/BlackJack/Card.h
+++ BlackJack/BlackJack/Card.h
@@ -15,7 +15,7 @@ public:
 	//ICard methods
 	EValue GetValue() const override;
 	ENumber GetNumber() const override;
-	static EValue ConvertNumberToValue(ENumber number);
+	
 
 	~Card();
 
@@ -24,3 +24,18 @@ private:
 	ENumber m_number;
 };
 
+static EValue ConvertNumberToValue(ENumber number)
+{
+	switch (number) {
+	case ENumber::A:
+		return EValue::eleven; //or 1 depending on the player
+	case ENumber::ten:
+	case ENumber::J:
+	case ENumber::Q:
+	case ENumber::K:
+		return EValue::ten;
+	default:
+
+		return static_cast<EValue>(number);
+	}
+}
