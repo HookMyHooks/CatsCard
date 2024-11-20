44d0632d1acb15726706810ac4a461bdd44978ae
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2024-11-20 11:18:21 +0200

    Added Card and Deck tests

---
+++ BlackJack/BlackJackTests/CardTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+
+#include "Card.h"
+
+TEST(CardValueGetter, isTrue) {
+	Card card(EValue::eight, ENumber::eight);
+	EValue value = card.GetValue();
+	ASSERT_EQ(value, EValue::eight);
+}
+
+TEST(CardNumberGetter, isTrue) {
+	Card card(EValue::four, ENumber::four);
+	ENumber number = card.GetNumber();
+	ASSERT_EQ(number, ENumber::four);
+}
---
+++ BlackJack/BlackJackTests/DeckTest.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+
+#include "Deck.h"
+
+TEST(DeckSize, isTrue) {
+	Deck* deck = new Deck();
+	int size = deck->GetCards().size();
+	ASSERT_EQ(size, 52);
+}
+
+TEST(DeckStructure, isTrue) {
+	Deck* deck = new Deck();
+	std::vector<std::shared_ptr<Card>> cards = deck->GetCards();
+    std::map<std::pair<ENumber, EValue>, int> cardCount;
+
+    for (auto& card : cards) {
+        std::pair<ENumber, EValue> cardKey = { card->GetNumber(), card->GetValue() };
+        cardCount[cardKey]++;
+    }
+
+    for (int rank = static_cast<int>(ENumber::A);
+        rank <= static_cast<int>(ENumber::K);
+        ++rank) {
+        ENumber rankEnum = static_cast<ENumber>(rank);
+        EValue value = ConvertNumberToValue(rankEnum);
+
+        std::pair<ENumber, EValue> expectedCard = { rankEnum, value };
+        ASSERT_EQ(cardCount[expectedCard], 4);
+    }
+}
+
+TEST(GiveCard, isTrue) {
+	Deck* deck = new Deck();
+	std::shared_ptr<Card> card = deck->GiveCard();
+	int size = deck->GetCards().size();
+	ASSERT_EQ(size, 51);
+}
